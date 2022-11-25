696f52e5e1df1600f1154c84fc4d04cf393a3c12
Nosrey <106562225+Nosrey@users.noreply.github.com>
2022-11-25 11:09:21 -0400

    primer commit

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Home() {
+    return (
+        <div>
+            <h1>Home screen</h1>
+        </div>
+    )
+}
---
+++ src/components/login/Login.jsx
@@ -0,0 +1,11 @@
+// import React from 'react';
+// import { Link } from 'react-router-dom';
+
+// export default funct
+// ion Login() {
+//     return (
+//         <div>
+//             <h1>Login screen</h1>
+//         </div>
+//     )
+// }
