07dfc47bb45724e3392309f39fe7fabf1277e145
Nosrey <106562225+Nosrey@users.noreply.github.com>
2022-12-19 22:46:16 -0400

    seccion de crear productos actualizada

--- src/components/crearProducto/crearProducto.jsx
+++ src/components/crearProducto/crearProducto.jsx
@@ -4,16 +4,42 @@ import { setForm, setProductos, filtrarProductos, setCategorias } from '../../ac
 import { connect } from "react-redux";
 import './crearProducto.css'
 
+
 let ready = true;
+
 function CrearProducto({ setForm, visible, setProductos, productos, filtrarProductos, categorias, setCategorias }) {
+
+    const imagenNotFound = 'https://www.freeiconspng.com/thumbs/no-image-icon/no-image-icon-6.png'
+    const paleta = ["text-fuchsia-400", "text-purple-500", "text-violet-500", "text-indigo-500", "text-blue-500", "text-sky-500", "text-cyan-500", "text-teal-500"]
+
+    const bordes = ["border-fuchsia-400", "border-purple-500", "border-violet-500", "border-indigo-500", "border-blue-500", "border-sky-500", "border-cyan-500", "border-teal-500"]
+
     const notFound = 'No existe esa categoria';
-    const [image, setImage] = useState('')
+    const [image, setImage] = useState(imagenNotFound)
     const [pcategory, setPcategory] = useState('')
     const [filterCategories, setFilterCategories] = useState('')
 
     function cerrar(e) {
         e.preventDefault();
         setForm()
+    }
+
+    function primeraMayuscula(palabra) {
+        if (palabra) {
+            let palabrita = palabra.toLowerCase().split('');
+            palabrita[0] = palabrita[0].toUpperCase();
+            return '"' + palabrita.join('') + '"'
+        }
+    }
+
+    function getRndInteger(min, max) {
+        return Math.floor(Math.random() * (max - min)) + min;
+    }
+
+    function colores(i) {
+        if (i > 7) i = getRndInteger(0, 8)
+        let color = paleta[(i - 7) * -1]
+        return color + ' inline m-1 border-2 ' + bordes[(i - 7) * -1] + ' px-1 py-0.5'
     }
 
     function comas(valorArray) {
@@ -41,55 +67,6 @@ function CrearProducto({ setForm, visible, setProductos, productos, filtrarProdu
         let filtro = categorias.find((el) => el.name.toLowerCase().includes(ultimoValor.toLowerCase()))
         if (!filtro || ultimoValor === '' || ultimoValor === ' ') filtro = { name: notFound }
         setFilterCategories(filtro.name)
-    }
-
-    function showUploadWidget(e) {
-        e.preventDefault();
-        window.cloudinary.openUploadWidget({
-            cloudName: "dyg5hutpb",
-            uploadPreset: "ejemplo",
-            sources: [
-                "local",
-                "url"
-            ],
-            googleApiKey: "<image_search_google_api_key>",
-            showAdvancedOptions: true,
-            //  multiple: false, debe ser eliminado para que funcione bien
-            //  cropping: true, esta tambien
-            defaultSource: "local",
-            styles: {
-                palette: {
-                    window: "#ffffff",
-                    sourceBg: "#f4f4f5",
-                    windowBorder: "#90a0b3",
-                    tabIcon: "#000000",
-                    inactiveTabIcon: "#555a5f",
-                    menuIcons: "#555a5f",
-                    link: "#0433ff",
-                    action: "#339933",
-                    inProgress: "#000519",
-                    complete: "#339933",
-                    error: "#cc0000",
-                    textDark: "#000000",
-                    textLight: "#fcfffd"
-                },
-                fonts: {
-                    default: null,
-                    "'Acme', sans-serif": {
-                        url: "https://fonts.googleapis.com/css?family=Acme",
-                        active: true
-                    }
-                }
-            }
-        },
-            (error, result) => {
-                if (!error && result && result.event === "success") {
-                    console.log('Done! Here is the image info: ', result.info.secure_url);
-                    setImage(result.info.secure_url)
-                }
-                else if (error) { console.log('parece que hubo un error: ', error) }
-            }
-        );
     }
 
     const handleSubmit = (e) => {
@@ -136,6 +113,16 @@ function CrearProducto({ setForm, visible, setProductos, productos, filtrarProdu
                 pstock.value = 0
             }
 
+            if (pname.value) {
+                let nombre = pname.value;
+                let existencia = productos.filter((el) => el.name.toLowerCase() === nombre.toLowerCase())
+                console.log('hola soy existencia: ', existencia.length)
+                if (existencia.length) {
+                    alert('El producto ingresado ya existe, por favor ingresa uno diferente')
+                    pname.value = '';
+                }
+            }
+
             if (pname.value && pstock.value && pprice.value) {
                 let salir = 0; // para disparar la salida
                 // eslint-disable-next-line
@@ -175,7 +162,7 @@ function CrearProducto({ setForm, visible, setProductos, productos, filtrarProdu
                         pstock.value = '';
                         pprice.value = '';
                         setPcategory('');
-                        setImage('');
+                        setImage(imagenNotFound);
                         setFilterCategories('');
                     })
                     .then(() => {
@@ -196,35 +183,81 @@ function CrearProducto({ setForm, visible, setProductos, productos, filtrarProdu
         };
     }
 
+
+    function handleFileChange(e) {
+        let file = e.target.files[0]
+
+        const data = new FormData();
+        data.append('file', file)
+        data.append('upload_preset', 'ejemplo')
+        data.append('cloud_name', 'dyg5hutpb')
+        fetch('https://api.cloudinary.com/v1_1/dyg5hutpb/image/upload',
+            {
+                method: 'post',
+                body: data
+            })
+            .then(resp => resp.json())
+            .then(data => {
+                setImage(data.secure_url)
+            })
+            .catch(err => console.log('hubo un error: ', err))
+
+    }
+
+
+
     return (
-        <div className={visible ? "fixed bg-white w-1/3 top-20 border-2 rounded-md border-l-0 hover:border-sky-200" : 'hidden'}>
-            <button className='bg-red-600 text-white absolute top-1 right-2 px-1.5 font-black hover:bg-red-300 text-xl' onClick={cerrar}>X</button>
+        <div className={visible ? "fixed bg-white w-96 top-0 border-2 rounded-md border-l-0 hover:border-sky-200 overflow-auto max-h-screen p-4 pt-1" : 'hidden'}>
+
+
+
+            <button className='bg-red-600 text-white absolute top-2 right-2 px-1.5 font-black hover:bg-red-300 text-xl' onClick={cerrar}>X</button>
             <div className="">
-                <form onSubmit={handleSubmit} autoComplete="off">
+                <form onSubmit={handleSubmit} autoComplete="off" className='flex flex-col items-center'>
                     <div className="input-container">
-                        <label className="text-lg font-semibold text-center">Nombre del producto</label>
-                        <input type="text" name="pname" placeholder='Nombre...' className=" mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-base shadow-sm placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500 disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none invalid:border-pink-500 invalid:text-pink-600 focus:invalid:border-pink-500 focus:invalid:ring-pink-500" required />
+                        <label className="text-xl font-semibold text-center">Nombre del producto</label>
+                        <input type="text" name="pname" placeholder='Nombre...' className=" mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-lg shadow-sm placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500 disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none invalid:border-pink-500 invalid:text-pink-600 focus:invalid:border-pink-500 focus:invalid:ring-pink-500" required />
 
                     </div>
                     <div className="input-container">
-                        <label className="text-lg font-semibold text-center">Cantidad Disponible</label>
-                        <input type="text" name="pstock" placeholder='Cantidad...' className=" mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-base shadow-sm placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500 disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none invalid:border-pink-500 invalid:text-pink-600 focus:invalid:border-pink-500 focus:invalid:ring-pink-500" required />
+                        <label className="text-xl font-semibold text-center">Cantidad Disponible</label>
+                        <input type="text" name="pstock" placeholder='Cantidad...' className=" mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-lg shadow-sm placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500 disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none invalid:border-pink-500 invalid:text-pink-600 focus:invalid:border-pink-500 focus:invalid:ring-pink-500" required />
 
                     </div>
                     <div className="input-container">
-                        <label className="text-lg font-semibold text-center">Precio del producto</label>
-                        <input id="price" type="text" name="pprice" placeholder="Precio..." className=" mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-base shadow-sm placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500 disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none invalid:border-pink-500 invalid:text-pink-600 focus:invalid:border-pink-500 focus:invalid:ring-pink-500" required />
+                        <label className="text-xl font-semibold text-center">Precio del producto</label>
+                        <input id="price" type="text" name="pprice" placeholder="Precio..." className=" mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-lg shadow-sm placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500 disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none invalid:border-pink-500 invalid:text-pink-600 focus:invalid:border-pink-500 focus:invalid:ring-pink-500" required />
 
                     </div>
-                    <div className="input-container">
-                        <label className="text-lg font-semibold text-center">Categorias</label>
-                        <input value={pcategory} onChange={handlePcategory} type="text" name="pcategory" placeholder="Categorias..." className=" mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-base shadow-sm placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500 disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none invalid:border-pink-500 invalid:text-pink-600 focus:invalid:border-pink-500 focus:invalid:ring-pink-500" required />
+                    <div className="input-container flex flex-col items-center">
+                        <label className="text-xl font-semibold text-center">Categorias</label>
+                        <input value={pcategory} onChange={handlePcategory} type="text" name="pcategory" placeholder="Categorias..." className=" mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-lg shadow-sm placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500 disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none invalid:border-pink-500 invalid:text-pink-600 focus:invalid:border-pink-500 focus:invalid:ring-pink-500" />
 
-                        <p>{(finalElement(pcategory).toLowerCase() === filterCategories.toLowerCase() && finalElement(pcategory).length) ? '' : (filterCategories === notFound) ? filterCategories : 'Sugerencia: ' + filterCategories}</p>
+                        <p>{(finalElement(pcategory).toLowerCase() === filterCategories.toLowerCase() && finalElement(pcategory).length) ? <p className='my-1'></p> : (filterCategories === notFound) ? <p className='my-1 text-gray-500 italic'>{filterCategories}</p> : <div className='my-1'><p className='inline text-gray-500 italic'>Sugerencia: </p><p className='inline'>{primeraMayuscula(filterCategories)}</p></div>}</p>
+
+                        <ul className='flex flex-wrap justify-center'>
+                            {
+                                (comas(pcategory).split(',')[0] !== '') ? (
+                                    comas(pcategory).split(',').map((el, i) => {
+                                        return (
+                                            <li className={colores(i)}>{primeraMayuscula(el)}</li>
+                                        )
+                                    })
+
+                                ) : ''
+                            }
+
+                        </ul>
+
                     </div>
-                    <button type="button" className='boton-imagen' onClick={showUploadWidget}>Subir imagen</button>
 
-                    <button type='submit' className='boton-imagen'>Crear producto</button>
+                    <label class="block">
+                        <span class="sr-only">Subir imagen</span>
+                        <input type="file" accept="image/x-png,image/gif,image/jpeg" onChange={handleFileChange} class="block w-full text-sm text-slate-500 file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-sm file:font-semibold file:bg-indigo-50 file:text-indigo-700 hover:file:bg-indigo-200 ml-2" />
+                    </label>
+
+                    <img src={image} alt='product' className='w-28 inline py-4' />
+                    <button type='submit' className='bg-sky-500 hover:bg-sky-600 text-white font-bold py-2 px-4 rounded text-xl'>Crear producto</button>
                 </form>
             </div>
         </div>
@@ -248,3 +281,4 @@ function mapDispatchToProps(dispatch) {
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(CrearProducto);
+
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -51,22 +51,6 @@ function Home({ mostrarForm, setForm, setProductos, productos, mostrarEdit, prod
             <CrearProducto visible={mostrarForm} />
             <CambiarProducto visible={mostrarEdit} />
 
-            {/* <form class="flex items-center space-x-6">
-                <div class="shrink-0">
-                    <img class="h-16 w-16 object-cover rounded-full" src="https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1361&q=80" alt="Current profile photo" />
-                </div>
-                <label class="block">
-                    <span class="sr-only">Choose profile photo</span>
-                    <input type="file" class="block w-full text-sm text-slate-500
-      file:mr-4 file:py-2 file:px-4
-      file:rounded-full file:border-0
-      file:text-sm file:font-semibold
-      file:bg-violet-50 file:text-violet-700
-      hover:file:bg-violet-100
-    "/>
-                </label>
-            </form> */}
-
             <div className='caja titulos'>
                 <h2 className='elemento titu'>Nombre</h2>
                 <h2 className='elemento'>Stock</h2>
