0ce063e57fbceb4f8524480bb660aad253991db6
Nosrey <106562225+Nosrey@users.noreply.github.com>
2022-12-30 17:22:04 -0400

    cambio del boton de retroceso

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Home from './components/home/Home.jsx'
 import React from 'react';
 
@@ -8,6 +8,7 @@ function App() {
     <div className='w-full'>
       <Routes>
         <Route exact path="/" element={<Home />} />
+        <Route path='*' element={<Navigate to ='/'/>}/>
       </Routes>
     </div>
   );
